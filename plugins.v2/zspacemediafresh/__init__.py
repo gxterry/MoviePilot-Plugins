7bdbd6972aa7104caef24ed991cc645332d7eb97
TerryGuo <gx.terry@163.com>
2024-11-30 21:33:42 +0800

    支持V2

--- plugins.v2/zspacemediafresh/__init__.py
+++ plugins.v2/zspacemediafresh/__init__.py
@@ -25,7 +25,7 @@ class ZspaceMediaFresh(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/gxterry/MoviePilot-Plugins/main/icons/Zspace_B.png"
     # 插件版本
-    plugin_version = "1.5.2"
+    plugin_version = "2.1.0"
     # 插件作者
     plugin_author = "gxterry"
     # 作者主页
